549abfc8b4cbb3fd8ed2c7748bceb2479f4b8c66
Antony Lemos <lemosantony@gmail.com>
2020-12-01 15:47:29 -0300

    initial commit ⚡

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simulação Epidemiológica (SIR) 🧟‍♂
+
+## ⚡ Autor
+
+**Djalma Antony Lemos Rodrigues**
+
+
+## 🤖 Como executar?
+
+⚠️ **Os comandos a seguir irão utilizar o OS Linux como referência.**
+
+⚠️ **O programa necessita de um arquivo contendo as entradas de dados, deste modo, você poderá utilizar o arquivo "data.txt".**
+
+Para executar a aplicação em sua máquina, execute os seguintes comandos:
+
+```bash
+  # Gerar o arquivo binário
+  $ gcc main.c -o main
+
+  # Executar o arquivo
+  $ ./main
+```
+
+Após a execução, será gerado um arquivo com o formato **CSV** com os dados obtidos.
+
+
+## ☹ Limitações
+
+O nome do arquivo de entrada de dados pecisa ser obrigatoriamente `data.txt`. Além disso, a apresentação atual dos valores (separados por um espaço) também precisará ser mantida.
---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int getPeriod(int days, int hours) {
+  return days * hours;
+}
+
+int main(void) {
+  FILE *file;
+  file = fopen("data.txt", "r");
+
+  if (file == NULL) {
+    printf("Houve um problema ao abrir o arquivo!\n");
+    
+    return 0;
+  }
+  
+  double S = 0, I = 0, R = 0, h = 0, N_b = 0, T_b = 0, S_bi = 0, I_bi = 0, m_k = 0, n_k = 0, T_k = 0;
+  int simulation_period = 0;
+
+  fscanf(file, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %i", &S, &I, &R, &h, &N_b, &T_b, &S_bi, &I_bi, &m_k, &n_k, &T_k, &simulation_period);
+
+  fclose(file);
+
+  simulation_period = getPeriod(simulation_period, 24);
+
+  double b = N_b / (T_b * S_bi * I_bi);
+  double k = m_k / (n_k * T_k);
+
+  double S_aux = S;
+  double I_aux = I;
+  double R_aux = R;
+  double time = 0;
+
+  FILE *csv_file = fopen("results.csv", "w+");
+
+  if (csv_file == NULL) {
+    printf("Houve um problema ao abrir o arquivo!\n");
+    
+    return 0;
+  }
+
+  fprintf(csv_file, "%lf, %lf, %lf, %lf\n", S, I, R, time);
+
+  while (time < simulation_period) {
+    S = S_aux - h * b * S_aux * I_aux;
+    I = I_aux + h * (b * S_aux * I_aux - k * I_aux);
+    R = R_aux + h * k * I_aux;
+    time = time + h;
+
+    fprintf(csv_file, "%lf, %lf, %lf, %lf\n", S, I, R, time);
+
+    S_aux = S;
+    I_aux = I;
+    R_aux = R;
+  }
+
+  fclose(csv_file);
+
+  return 0;
+}
